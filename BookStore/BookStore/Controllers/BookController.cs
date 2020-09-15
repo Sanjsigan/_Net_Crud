b733400d733afed876c6bedd32f6684c183661cb
Sanjsigan <sanjsi0507@gmail.com>
2020-09-15 13:42:21 +0530

    Create Json Delete

--- BookStore/BookStore/Controllers/BookController.cs
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookStore.Model;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Controllers
 {
     [Route("api/Book")]
+
     [ApiController]
     public class BookController : Controller
     {
@@ -27,5 +28,22 @@ namespace BookStore.Controllers
         {
             return Json(new { data = _db.Book.ToList() });
         }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookfromDb =await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookfromDb == null)
+            {
+                return Json(new { success = false, message = "Erroe while Deleting" });
+                
+            }
+            _db.Book.Remove(bookfromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successfull" });
+
+        }
+        
     }
 }
